08b3dc00c6a35edbf285a19e00ff9a61df0c28d4
CHAITU SAINI <CHAITU SAINI@CHAITU>
2017-07-27 11:53:12 -0700

    Whileloops

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int value = 10;
+
+		boolean loop = 4 < 5;
+
+		boolean loop1 = value > 20;
+
+		System.out.println(loop);
+
+		System.out.println(loop1);
+
+		int value1 = 0;
+
+		while (value1 < 10) {
+			System.out.println("Hello" + value1);
+			value1 = value1 + 1;
+		}
+	}
+}
